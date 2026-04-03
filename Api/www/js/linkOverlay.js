46beaea8cc4b977cdb17aecd4efe6284b9578660
Arina Kiskina <ae.kiskina@gmail.com>
2026-04-03 18:38:18 +0500

    project page

---
+++ Api/www/js/linkOverlay.js
@@ -0,0 +1,31 @@
+﻿function openOverlay() {
+    document.querySelector('.overlay').style.display = 'block';
+    document.querySelector('.overlay-bg').style.display = 'block';
+    document.body.style.overflow = 'hidden';
+}
+
+function closeOverlay() {
+    document.querySelector('.overlay').style.display = 'none';
+    document.querySelector('.overlay-bg').style.display = 'none';
+    document.body.style.overflow = '';
+}
+
+function copyLink(id) {
+    const input = document.getElementById(id);
+    input.select();
+    input.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(input.value).then(() => {
+        alert("Ссылка скопирована!");
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('linkOverlay.html')
+        .then(response => response.text())
+        .then(html => {
+            const container = document.createElement('div');
+            container.innerHTML = html;
+            document.body.appendChild(container);
+        })
+        .catch(err => console.error('Ошибка загрузки оверлея:', err));
+});
---
+++ Api/www/js/project.js
@@ -0,0 +1,53 @@
+﻿const projects = [
+    {
+        id: 'vibik',
+        title: "Vibik",
+        description: "Приложение с фото-заданиями, которое мотивирует выйти из рутины и прогуляться.",
+        stack: [
+            "python",
+            "csharp",
+            "postgresql",
+            "docker"
+        ]
+    },
+
+    {
+        id: 'matmeh-maps',
+        title: "Матмех мэпс",
+        description: "Приложение — навигатор по матмеху",
+        stack: [
+            "csharp",
+            "docker",
+            "javascript",
+            "css3"
+        ]
+    }
+];
+
+
+const params = new URLSearchParams(window.location.search);
+const projectId = params.get("id");
+
+
+const project = projects.find(p => p.id === projectId);
+
+
+if(project){
+
+    document.getElementById("project-title").textContent = project.title;
+
+    document.getElementById("short-description").textContent = project.description;
+
+    const stackContainer = document.getElementById("stack-icons");
+
+    project.stack.forEach(tech => {
+
+        const img = document.createElement("img");
+
+        img.src = `https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${tech}/${tech}-original.svg`;
+
+        stackContainer.appendChild(img);
+
+    });
+
+}
