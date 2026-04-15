05704a16b959a122d239b9f9309f4e3efdc05a86
Arina Kiskina <ae.kiskina@gmail.com>
2026-04-15 15:03:34 +0500

    api path fixed

--- Api/wwwroot/js/api/projectApi.js
+++ Api/wwwroot/js/api/projectApi.js
@@ -1,6 +1,6 @@
 ﻿export async function getProject(id) {
 
-    const response = await fetch(`http://localhost:5100/api/projects/${id}`);
+    const response = await fetch(`http://213.165.213.101:8080/api/projects/project/${id}`);
 
     if (!response.ok) {
         throw new Error("Failed to fetch project");
