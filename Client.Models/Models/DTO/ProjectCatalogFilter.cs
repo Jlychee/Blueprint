4b63f08c9132547711d2dc6dda404848ac5b5e51
fan4cz <fan4cz@gmail.com>
2026-04-17 12:28:11 +0500

    tag ids

--- Client.Models/Models/DTO/ProjectCatalogFilter.cs
+++ Client.Models/Models/DTO/ProjectCatalogFilter.cs
@@ -3,7 +3,8 @@
 public sealed class ProjectCatalogFilter
 {
     public string? Search { get; init; }
-    public List<int>? TagIds {get; init;}
+    public List<int>? TagIds {get; set;}
+    public List<string>? TagNames {get; init;}
     public int? TeamMemberCount { get; init; }
     public int? Year {get; init;}
     public int? Semester {get; init;}
