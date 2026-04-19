d40c3401ec2bdd59218d1f3662015d2b68c69916
Илья Котов <roskets801@gmail.com>
2026-04-19 17:18:37 +0500

    добавил колонку в таблицу

--- Infrastructure/Parsers/CsvProjectParser.cs
+++ Infrastructure/Parsers/CsvProjectParser.cs
@@ -40,20 +40,21 @@ public class CsvProjectParser(ITagRepository tagRepository) : IProjectTableParse
         var rawYear = fields[6];
         var rawSemester = fields[7];
         var shortDescription = fields[8];
-        var rawDescriptionLink = fields[9];
-        var rawCastDevLink = fields[10];
-        var rawMvpLink = fields[11];
-        var rawRoadMapLink = fields[12];
-        var rawGitLink = fields[13];
-        var rawNonGitLink = fields[14];
-        var rawTagsJson = fields[15];
+        var description = fields[9];
+        var rawDescriptionLink = fields[10];
+        var rawCastDevLink = fields[11];
+        var rawMvpLink = fields[12];
+        var rawRoadMapLink = fields[13];
+        var rawGitLink = fields[14];
+        var rawNonGitLink = fields[15];
+        var rawTagsJson = fields[16];
 
         return new FullProjectInfo
         {
             Name = name,
             Year = int.Parse(rawYear),
             Semester = int.Parse(rawSemester),
-            Description = shortDescription,
+            Description = description,
             ShortDescription = shortDescription,
             TeamMembers = ParseTeamMembers(rawParticipantFields),
             Files = ParseFiles(rawDescriptionLink, rawCastDevLink, rawMvpLink, rawRoadMapLink, rawGitLink,
