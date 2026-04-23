35bb23e3e44d436df366d77113c2a91026cb323d
fan4cz <fan4cz@gmail.com>
2026-04-23 20:07:10 +0500

    fix

--- Client.Models/Models/DTO/ProjectCatalogFilter.cs
+++ Client.Models/Models/DTO/ProjectCatalogFilter.cs
@@ -3,8 +3,7 @@
 public sealed class ProjectCatalogFilter
 {
     public string? Search { get; init; }
-    public List<int>? TagIds {get; set;}
-    public List<string>? TagNames {get; init;}
+    public List<int>? TagIds {get; init;}
     public int? TeamMemberCount { get; init; }
     public int? Year {get; init;}
     public int? Semester {get; init;}
