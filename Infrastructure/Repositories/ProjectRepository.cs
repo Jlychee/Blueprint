e6466ed8c72c3c404109c31c9b77b9bc6c4821e4
Lychee <2006tka@gmail.com>
2026-05-06 19:44:04 +0500

    OrderByDescending (Likes -> Likes.Count)

--- Infrastructure/Repositories/ProjectRepository.cs
+++ Infrastructure/Repositories/ProjectRepository.cs
@@ -179,7 +179,7 @@ public class ProjectRepository(ProjectContext projectContext) : IProjectReposito
         var total = await query.CountAsync(ct);
 
         var items = await query
-            .OrderByDescending(p => p.Likes)
+            .OrderByDescending(p => p.Likes.Count)
             .Skip((filter.Page - 1) * filter.PageSize)
             .Take(filter.PageSize)
             .Select(p => new ProjectCardDto
